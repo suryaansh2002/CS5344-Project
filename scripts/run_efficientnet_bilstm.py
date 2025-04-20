b898e7b9cc55e94509851ef97bee2d66aa97d1b4
sneha <30837089+itmesneha@users.noreply.github.com>
2025-04-21 04:08:20 +0800

    report updated

--- scripts/run_efficientnet_bilstm.py
+++ scripts/run_efficientnet_bilstm.py
@@ -61,7 +61,8 @@ def text_pipeline(text):
 
 image_transform = transforms.Compose([
     transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 logging.info("Tokenizer and image transform initialized.")
 
